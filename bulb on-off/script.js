a03d00301a134b8db18fbefb34f26509e7edc4f6
Muhammad-Uzair8 <m.uzairb6966@gmail.com>
2024-07-18 16:06:35 +0500

    Add files via upload

---
+++ bulb on-off/script.js
@@ -0,0 +1,8 @@
+// Turn on function
+ function bulbOn(){
+    document.getElementById("bulb").src="bulb-on.png";
+}
+// Turn off function
+function bulbOff(){
+    document.getElementById("bulb").src="bulb-off.png";
+}
